04713ca9840aea9931e0f11e560e597648dea455
Noeri Huisman <8823461+mrxz@users.noreply.github.com>
2024-10-31 08:41:52 +0100

    perf: Cache ancestor list instead of recomputing every time

--- packages/three-vrm-springbone/src/utils/traverseAncestorsFromRoot.ts
+++ packages/three-vrm-springbone/src/utils/traverseAncestorsFromRoot.ts
@@ -1,15 +1,21 @@
 import type * as THREE from 'three';
 
 export function traverseAncestorsFromRoot(object: THREE.Object3D, callback: (object: THREE.Object3D) => void): void {
-  const ancestors: THREE.Object3D[] = [];
+  let ancestors: THREE.Object3D[] = object.userData.ancestors;
 
-  let head: THREE.Object3D | null = object;
-  while (head !== null) {
-    ancestors.unshift(head);
-    head = head.parent;
+  if(!ancestors) {
+    ancestors = [];
+
+    let head: THREE.Object3D | null = object;
+    while (head !== null) {
+      ancestors.unshift(head);
+      head = head.parent;
+    }
+
+    object.userData.ancestors = ancestors;
   }
 
-  ancestors.forEach((ancestor) => {
-    callback(ancestor);
-  });
+  for(let i = 0; i < ancestors.length; i++) {
+    callback(ancestors[i]);
+  }
 }
